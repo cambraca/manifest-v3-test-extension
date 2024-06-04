f7bfd24bfba4ff0a95ceec60e4d3f6930e73cda3
Camilo Bravo <hola@cambraca.com>
2024-06-04 11:44:03 -0600

    use Cite.async in an attempt to use the Fetch API and not XMLHttpRequest, which is not available in service workers

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ import '@citation-js/plugin-doi';
 
 await new Promise(r => setTimeout(r, 1000));
 
-let example = new Cite('10.1038/s41467-021-25015-6')
+const doi = '10.1038/s41467-021-25015-6';
+let example = await Cite.async(doi, {forceType: "@doi/id"});
 
 let output = example.format('bibliography', {
     format: 'html',
